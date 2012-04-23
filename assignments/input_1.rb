788b8b761c0ade4ff0a7e51040eb63671a5804a0
Dessy Daskalov <dessy.daskalov@gmail.com>
2012-04-22 22:21:46 -0400

    Add Mad Libs games

---
+++ assignments/input_1.rb
@@ -0,0 +1,9 @@
+puts "Hi there, my name is Ruby Robot. What's your name?"
+
+name = gets.chomp
+
+puts "Nice to meet you " + name + "! How old are you?"
+
+age = gets.chomp
+
+puts "You're " + age + "?! Wow, I'm 10 times older than you. I'm " + age + "0!"
---
+++ assignments/input_3.rb
@@ -0,0 +1,50 @@
+puts "Let's play a game of Mad Libs! When I ask you for a type of word, type it in."
+
+puts "Adjective:"
+adjective_1 = gets.chomp
+
+puts "Plural Noun:"
+plural_noun = gets.chomp
+
+puts "Part of the body:"
+part_of_the_body = gets.chomp
+
+puts "Adjective:"
+adjective_2 = gets.chomp
+
+puts "Adjective:"
+adjective_3 = gets.chomp
+
+puts "Adjective:"
+adjective_4 = gets.chomp
+
+puts "Adjective:"
+adjective_5 = gets.chomp
+
+puts "Noun:"
+noun_1 = gets.chomp
+
+puts "Noun:"
+noun_2 = gets.chomp
+
+puts "Verb:"
+verb = gets.chomp
+
+puts "Verb (Past Tense):"
+verb_past_tense = gets.chomp
+
+puts
+puts "Pirates love to sing " + adjective_1 + " " + plural_noun + " called limericks."
+puts "Here are two favorites, usually on the tip of every pirate's " + part_of_the_body + ":"
+puts
+puts "A pretty, " + adjective_1 + " pirate named Alice"
+puts "Broke into the king's " + adjective_3 + " palace."
+puts "She caused a " + adjective_4 + " scene,"
+puts "But she soon became queen--"
+puts "That pretty, " + adjective_5 + " pirate of malice."
+puts
+puts "There once was a " + noun_1 + " named Billy."
+puts "His " + noun_2 + "-mates considered him quite silly."
+puts "He'd " + verb + " up the mast"
+puts "While shouting, 'Avast!'"
+puts "As the ship " + verb_past_tense + " up to Philly."
