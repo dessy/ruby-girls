1002d834c9050d69e7cc32dc7fde61f4a61f3401
Dessy Daskalov <dessy.daskalov@gmail.com>
2012-04-30 00:55:16 -0400

    Add final logic assignment

--- assignments/logic_2.rb
+++ assignments/logic_2.rb
@@ -1,11 +1,50 @@
-puts "I'm thinking of a secret word. It's an animal. Try and guess it!"
-guess = gets.chomp
+puts "Hi there. I'm Ruby Robot. Since I'm a robot, I don't have a birthday. Which month is your birthday in?"
+birthday = gets.chomp
 
-if guess == "sheep"
-  puts "Great work! You guessed correctly!"
+if birthday.downcase == "january"
+  puts "January is all about New Year's resolutions, and not breaking them!"
 end
 
-if guess != "sheep"
-  puts "Nice try, but that's the not word I was thinking of. Try again!"
-  system "ruby assignments/logic_2.rb"
+if birthday.downcase == "february"
+  puts "February has 28 days, except for in a leap year, when there are 29 days."
+end
+
+if birthday.downcase == "march"
+  puts "March is when spring begins! The official first day of spring is March 20th."
+end
+
+if birthday.downcase == "april"
+  puts "April finally brings warm weather!"
+end
+
+if birthday.downcase == "may"
+  puts "May, the month that Girls Crack the Code is held this year!"
+end
+
+if birthday.downcase == "june"
+  puts "School's out for the summer!"
+end
+
+if birthday.downcase == "july"
+  puts "July is the month we celebrate Canada Day."
+end
+
+if birthday.downcase == "august"
+  puts "August brings great camping weather. Spend some time outside on your birthday!"
+end
+
+if birthday.downcase == "september"
+  puts "We all go back to school in September, after the Labour Day long weekend."
+end
+
+if birthday.downcase == "october"
+  puts "October is the time to dress up as someone (or something) else, and celebrate Halloween."
+end
+
+if birthday.downcase == "november"
+  puts "Dull November brings the blast, then the leaves are whirling fast."
+end
+
+if birthday.downcase == "december"
+  puts "December is all about the holiday season!"
 end
